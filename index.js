63436eded5798d16f1324ff2a36175a8bcca99b2
Harrison Bishop <harrisonbishsp@gmail.com>
2022-11-07 10:20:40 +0000

    js file, added DOM manipulation to add one div, inside my html container for divs

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+let squares = document.createElement("div");
+squares.style.width = "16px";
+squares.style.height = "16px";
+squares.style.border = "solid";
+
+document.getElementById("container").appendChild(squares);
