6c389d6e6531ef12719b6d0f562a8f53105d2d0e
Harrison Bishop <harrisonbishsp@gmail.com>
2022-11-07 10:46:06 +0000

    added 16 vertical aligned boxes through DOM and a for loop

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-
-
-let squares = document.createElement("div");
+let masterDiv = document.getElementById("container");
+const squares = document.createElement('div');
 squares.style.width = "16px";
 squares.style.height = "16px";
 squares.style.border = "solid";
+squares.style.display = "flex";
 
-document.getElementById("container").appendChild(squares);
+for (let i=0; i<16; i++){
+masterDiv.appendChild(squares.cloneNode(true))
+}
+
+
+
+
+
+
+
+
+// for (let i=0; i<=16; i++){
+    
+// let squares = document.createElement("div");
+
+// for (let j=0; j<=16; j++){
+// document.getElementById("container").appendChild(squares);
+// }
+// }
+
+
