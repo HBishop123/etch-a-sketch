9708d120b32f27f2c80d0df36b292a8186a30f30
Harrison Bishop <harrisonbishsp@gmail.com>
2022-11-08 15:42:42 +0000

    grid of squares now changes when you move the slider

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //constants and styles for the grid
+
 let masterDiv = document.getElementById("container");
 const squares = document.createElement('div');
 
+//constants for the slider
+
+const slider = document.getElementById("myRange");
+const sliderAmount = document.getElementById("gridSize")
+
 // main container
+
 masterDiv.style.display = "grid";
 masterDiv.style.border = "solid"
 let masterHeight = 700;
@@ -10,27 +17,33 @@ let masterWidth = 700;
 masterDiv.style.height = masterHeight + 'px';
 masterDiv.style.width = masterWidth + 'px';
 
-
 //squares
+
 squares.style.border = "solid black";
 squares.style.backgroundColor = "white";
 squares.style.borderWidth = "1px";
 
-
-
-
-//constants for the slider
-const slider = document.getElementById("myRange");
-const sliderAmount = document.getElementById("gridSize")
+//squares cont
+let currentValue = slider.value;
+let squareHeight = masterHeight / currentValue
+let squareWidth = masterWidth / currentValue
+squares.style.height = squareHeight;
+squares.style.height = squareWidth;
 
 
 
 //creating grid based off slider value
+
 for (let j=0; j<slider.value; j++){
 for (let i=0; i<slider.value; i++){
 masterDiv.appendChild(squares.cloneNode(true))
 }
 }
+
+//changes grid depending on number of squares
+masterDiv.style.gridTemplateColumns = `repeat(${currentValue}, 1fr)`;
+masterDiv.style.gridTemplateRows = `repeat(${currentValue}, 1fr)`;
+
 
 //event listener for hover over grid = change colour
 masterDiv.addEventListener('mouseover', (e) => {
@@ -47,16 +60,16 @@ slider.oninput = function() {
 }
 })
 
-
+slider.addEventListener("change", () => {
+masterDiv.innerHTML = ""
 let currentValue = slider.value;
-
-//squares cont
-
-let squareHeight = masterHeight / currentValue
-let squareWidth = masterWidth / currentValue
-squares.style.height = squareHeight;
-squares.style.height = squareWidth;
-
 
 masterDiv.style.gridTemplateColumns = `repeat(${currentValue}, 1fr)`;
 masterDiv.style.gridTemplateRows = `repeat(${currentValue}, 1fr)`;
+
+for (let j=0; j<currentValue; j++){
+for (let i=0; i<currentValue; i++){
+masterDiv.appendChild(squares.cloneNode(true))
+}}
+
+})
